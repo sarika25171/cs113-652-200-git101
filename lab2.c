41686b6f6161f091607b4042f1df007dcd436bd5
sarika <sarika.o@ku.th>
2023-01-28 22:46:25 +0700

    add lab2.c

---
+++ lab2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int i;
+int main()
+{
+    //int j = 0;
+    // printf("%d %d\n", i, j);
+    // char str[10];
+    // gets(str);
+    // fgets(str, 10, stdin);
+    // printf("str : %s", str);
+    // terminate charactor, null char '\0'
+
+
+    // int i = atoi(str);
+    // long l = atol(str);
+    // double d = atof(str);
+
+    // printf("%d - %ld - %lf\n", i, l, d);
+    // printf("%c\n", c);
+
+
+
+    // int isDigit = 0;
+    // float isExist = 0.0;
+    // char isnumber = '\0';
+
+    // if (isDigit)
+    // {
+    //     printf("if %d\n", isDigit > 50);
+    // }else {
+    //     printf("else\n");
+    // }
+
+    char str[10];
+    printf("Enter your score : ");
+    fgets(str, 10, stdin);
+
+
+    // double score = atof(str);
+    // // relational operators > < <= >=
+    // if (score >= 80){
+    //     printf("A\n");
+    // }else if (score >= 70) {
+    //     printf("B\n");
+    // }else {
+    //     printf("F\n");
+    // }
+
+    // logical operators && || !
+    if (score < 50){
+        printf("F\n");
+    }else if (score >= 50 && score < )//พิมไม่ทันจาร
+    
+
+
+    printf("score : %lf\n", score);
+
+}
